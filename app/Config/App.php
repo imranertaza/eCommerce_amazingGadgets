d95485f376cdeab23edfe9478e5ada84d3309c99
Syed Imran Ertaza <imranertaza12@gmail.com>
2023-05-11 20:09:47 +0600

    .gitignore is now working

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://test.dns/ecommerce/public_html/';
+    public $baseURL = '';
 
     /**
      * --------------------------------------------------------------------------
