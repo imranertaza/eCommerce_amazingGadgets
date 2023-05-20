44f07d96b5636d8b42a2776303811a6a7ad19911
Syed Imran Ertaza <imranertaza12@gmail.com>
2023-05-20 19:23:27 +0600

    changed pager and env file

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,7 +24,7 @@ class Pager extends BaseConfig
         'default_full'   => 'CodeIgniter\Pager\Views\default_full',
         'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
         'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-        'custome_link'   => 'Pagers\custome_link',
+        'custome_link'   => 'App\Views\Pagers\custome_link',
     ];
 
     /**
